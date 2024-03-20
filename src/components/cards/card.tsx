64d55c5de26e61cf72ac32691cb503a5c9d2eda5
Quemia <que.carol@hotmail.com>
2024-03-19 22:08:24 -0300

    update

---
+++ src/components/cards/card.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const Card = () => {
+  return (
+    <>
+      <div>tarefas diárias</div>
+      <div className="card">
+        <div>
+          <div>
+            <p>Leitura</p>
+            <h3>O Milagre da manhã</h3>
+            <p>30min durante 15 dias</p>
+          </div>
+          <div>
+            <p>12%</p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Card;
--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,34 +1,12 @@
 import React from "react";
+import Card from "../../components/cards/card";
 
 const Home = () => {
   return (
     <>
       <div>tarefas diárias</div>
-      <div className="card">
-        <div>
-          <div>
-            <p>Leitura</p>
-            <h3>O Milagre da manhã</h3>
-            <p>30min durante 15 dias</p>
-          </div>
-          <div>
-            <p>12%</p>
-          </div>
-        </div>
-      </div>
-
-      <div className="card">
-        <div>
-          <div>
-            <p>Meditação</p>
-            <h3>Respiração</h3>
-            <p>5min de meditação</p>
-          </div>
-          <div>
-            <p>100%</p>
-          </div>
-        </div>
-      </div>
+      <Card />
+      <Card />
     </>
   );
 };
