1bbef495362c294f4a6d41c2e786fcdc18f8d147
Quemia <que.carol@hotmail.com>
2024-03-23 21:28:55 -0300

    update

--- src/components/cards/card.style.tsx
+++ src/components/cards/card.style.tsx
@@ -1,12 +1,21 @@
 import styled from "styled-components";
 
-export const CardContainer = styled.div`
+export type StyledCardProps = {
+  disabled: boolean;
+  bgColor: boolean;
+};
+
+export const CardContainer = styled.div<StyledCardProps>`
   width: 327px;
-  height: 115px;
+  height: 100px;
   padding: 16px;
 
   border: 1px solid #dcdde0;
   border-radius: 8px;
+
+  opacity: ${(props) => {
+    return props.disabled ? ".65" : "1";
+  }};
 
   display: flex;
   justify-content: space-between;
@@ -17,27 +26,63 @@ export const CardBoxText = styled.div`
   display: block;
 `;
 
-export const BoxTask = styled.div`
-  width: 80px;
+export const BoxTask = styled.div<StyledCardProps>`
+  max-width: 133px;
   height: 28px;
   border: transparent;
   border-radius: 24px;
-
-  background-color: #f1f2ff;
-  color: #315ce0;
 
   display: flex;
   text-align: center;
   justify-content: center;
   align-items: center;
 
-  margin-bottom:12px;
+  margin-bottom: 12px;
+
+  opacity: ${(props) => {
+    return props.disabled ? ".65" : "1";
+  }};
+
+  background-color: ${(props) => {
+    return props.bgColor ? "#f4f4f4" : "#fff";
+  }};
+
+  /* background-color: #f4f4f4; */
+  /* color: #6a7385; */
+`;
+
+export const TaskRead = styled(BoxTask)`
+  width: 80px;
+  color: #315ce0;
+
+  background-color: ${(props) => {
+    return props.bgColor ? "#f4f4f4" : "#f1f2ff";
+  }};
+`;
+
+export const TaskMeditate = styled(BoxTask)`
+  width: 99px;
+  /* background-color: #ffeda830; */
+  color: #bc8321;
+
+  background-color: ${(props) => {
+    return props.bgColor ? "#f4f4f4" : "#f1f2ff";
+  }};
+`;
+
+export const TaskSleepSound = styled(BoxTask)`
+  /* color: #f02222; */
+  color: #6a7385;
+
+  background-color: ${(props) => {
+    return props.bgColor ? "#f4f4f4" : "#ffab9c30";
+  }};
 `;
 
 export const BoxTaskText = styled.p`
   font: normal 600 12px/140% "Ubuntu", system-ui;
   margin-left: 4px;
-
+  opacity: none;
 `;
 
 export const TaskTitle = styled.h3`
@@ -54,6 +99,8 @@ export const TaskTime = styled.p`
 
 export const CardBoxEvolution = styled.div`
   display: block;
+  font: normal 600 12px/140% "Ubuntu", system-ui;
+  color: #7e98ec;
 `;
 
 export const BoxEvolution = styled.circle`
