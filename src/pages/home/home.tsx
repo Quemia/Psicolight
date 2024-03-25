24a0d8f22c76421e782a4c3cb95acc52195c5daf
Quemia <que.carol@hotmail.com>
2024-03-24 21:02:08 -0300

    update home

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,13 +1,59 @@
 import React from "react";
 import Card from "../../components/cards/card";
+import {
+  Container,
+  HomeHeader,
+  BoxHeader,
+  HeaderTitle,
+  HeaderIcon,
+  HeaderAvatar,
+} from "./home.style";
 
 const Home = () => {
   return (
-    <>
-      <div>tarefas diárias</div>
-      <Card />
-      <Card />
-    </>
+    <Container>
+      <HomeHeader>
+        <BoxHeader>
+          <HeaderIcon />
+          <HeaderTitle>Tarefas diárias</HeaderTitle>
+          <HeaderAvatar alt="avatar" src="./Perfil.png" />
+        </BoxHeader>
+      </HomeHeader>
+      <div>
+        <Card
+          task="Leitura"
+          title="O Milagre da manhã"
+          time="30 min durante 15 dias"
+          disabled={false}
+          bgColor={false}
+        />
+        <Card
+          task="Meditação"
+          title="Respiração"
+          time="5 min de meditação "
+          disabled={false}
+          bgColor={false}
+        />
+
+        <Card
+          task="Som para dormir"
+          title="Sons relaxantes"
+          time="Ouça até adormecer"
+          disabled={false}
+          bgColor={false}
+        />
+      </div>
+      <div>
+        <h2>Tarefas Bloqueadas</h2>
+        <Card
+          task="Cursos"
+          title="Orátoria e dicção"
+          time="Diego ainda não avaliou"
+          disabled={true}
+          bgColor={true}
+        />
+      </div>
+    </Container>
   );
 };
 
